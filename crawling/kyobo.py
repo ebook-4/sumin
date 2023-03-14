9bed67631282e969f506265255aecf31802a3a03
sumin895 <iris8085@naver.com>
2023-03-14 14:56:06 +0900

    23-03-14 check the log.md

--- crawling/kyobo.py
+++ crawling/kyobo.py
@@ -23,8 +23,12 @@ categories = []
 for data in datas :
     li = data.find_elements(By.CSS_SELECTOR, '.sub_depth_list a')
     for i in range(len(li)) :
-        if li[i].text != '' :
+        if li[i].text != '' and li[i].text != '섹슈얼로맨스' and li[i].text != '웹툰':
             categories.append(li[i].text)
+
+# for i in range(len(categories)) :
+#     print(categories[i])
+
 
 isbns = []
 books = []
@@ -42,41 +46,47 @@ for category in categories :
         soup = BeautifulSoup(html, 'html.parser')
         
         data = soup.select('.prodDt_detail')
-        contents = driver.find_elements(By.CSS_SELECTOR, '.prodDt_detail')
+        contents = driver.find_elements(By.CSS_SELECTOR, '.prodDt')
 
         # 상세페이지 주소를 통한 isbn
         for link in data :
-            detail = link.select_one('strong h3 a').attrs['href']
-            driver2.get(detail)
-            isbn = driver2.find_elements(By.CSS_SELECTOR, '.prod_pordInfo_box em')[1].text
-            isbns.append(isbn)
+            try :
+                detail = link.select_one('strong h3 a').attrs['href']
+                driver2.get(detail)
+                isbn = driver2.find_elements(By.CSS_SELECTOR, '.prod_pordInfo_box em')[1].text
+                isbns.append(isbn)
+            except :
+                isbns.append('0000000000000')
 
         # 전체 목록에서 그 외 정보 추출
         for content in contents :
-            title = content.find_element(By.CSS_SELECTOR, 'strong h3').text
-            author = content.find_element(By.CSS_SELECTOR, '.prodDt_info').text.split(' ')[0]
-            price = content.find_element(By.CSS_SELECTOR, '.prodDt_price b').text
+            img = content.find_element(By.CSS_SELECTOR, '.prodDt_cover img').get_attribute('src')
+            title = content.find_element(By.CSS_SELECTOR, '.prodDt_detail strong h3').text
+            author = content.find_element(By.CSS_SELECTOR, '.prodDt_detail .prodDt_info').text.split(' ')[0]
+            price = content.find_element(By.CSS_SELECTOR, '.prodDt_detail .prodDt_price b').text
             try :
-                star = content.find_element(By.CSS_SELECTOR, '.prodDt_review b').text
+                star = content.find_element(By.CSS_SELECTOR, '.prodDt_detail .prodDt_review b').text
             except :
                 star = "0.0"
-            books.append([title, author, price, star, category])
+            books.append([title, author, price, star, category, img])
 
         # 다음 페이지 이동
+        driver.execute_script('window.scrollTo(0,100000000)')
         driver.find_element(By.CSS_SELECTOR, '.btn_page.next').click()
 
 
-# 데이터 프레임 생성
-df = pd.DataFrame(columns=['ISBN', 'title', 'author', 'price', 'star', 'category'])
+# # 데이터 프레임 생성
+# df = pd.DataFrame(columns=['ISBN', 'title', 'author', 'price', 'star', 'category', 'img'])
 
-for i in range(len(isbns)) :
-    df.loc[i] = {
-        "ISBN" : isbns[i],
-        "title" : books[i][0],
-        "author" : books[i][1],
-        "price" : books[i][2],
-        "star" : books[i][3],
-        "category" : books[i][4]
-    }
+# for i in range(len(isbns)) :
+#     df.loc[i] = {
+#         "ISBN" : isbns[i],
+#         "title" : books[i][0],
+#         "author" : books[i][1],
+#         "price" : books[i][2],
+#         "star" : books[i][3],
+#         "category" : books[i][4],
+#         "img" : books[i][5],
+#     }
 
-df.to_csv('kyobo.csv', index=False, encoding='cp949')
+# df.to_csv('kyobo.csv', index=False, encoding='cp949')
